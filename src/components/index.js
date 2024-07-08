46b14fa843c28d62166a89b300461b6599646354
tetriv0217 <tetriv0217@gmail.com>
2024-07-09 04:40:00 +0530

    Just Doing the deployment

---
+++ src/components/index.js
@@ -0,0 +1,34 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from './Logo'
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from './Button'
+import Input from './Input'
+import Select from './Select'
+import RTE from "./RTE";
+import Signup from './Signup';
+import Login from './Login';
+import Protected from './AuthLayout'
+import PostForm from './post-form/PostForm';
+import PostCard from './PostCard'
+import AddPost from "../pages/AddPost";
+import AuthLayout from './AuthLayout';
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    RTE,
+    Signup,
+    Login,
+    Protected,
+    PostForm,
+    PostCard,
+    AddPost,
+    AuthLayout
+}
